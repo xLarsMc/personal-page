c541f8b0c1960508adcda90f75d52d82b24a2ce9
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-09-19 10:18:18 -0300

    Configuração básica da estrutura da página

--- personal-page/src/App.jsx
+++ personal-page/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Personal Page</h1>
+      <div className="app">
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <div className="container">
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+          </Routes>
+          </div>
+          <Footer></Footer>
+        </BrowserRouter>
       </div>
     </>
   )
